82b6f72bc457d7b9a7b05b299c0692acb124125e
millingermarkus <markus.millinger@chalmers.se>
2021-09-20 16:39:07 +0200

    Added update of data for existing nuclear powerplants

--- scripts/build_powerplants.py
+++ scripts/build_powerplants.py
@@ -81,6 +81,8 @@ import numpy as np
 
 from scipy.spatial import cKDTree as KDTree
 
+from six import iteritems
+
 logger = logging.getLogger(__name__)
 
 
@@ -94,6 +96,98 @@ def add_custom_powerplants(ppl):
         add_ppls.query(custom_ppl_query, inplace=True)
     return ppl.append(add_ppls, sort=False, ignore_index=True, verify_integrity=True)
 
+
+def correct_nuclear_data(ppl):
+    # Data from https://en.wikipedia.org/wiki/List_of_commercial_nuclear_reactors
+    YearCommercial = {
+        'St laurent': 1983,
+        'Gravelines': 1985,
+        'Paluel': 1986,
+        'Penly': 1992,
+        'Nogent': 1989,
+        'Golfech': 1994,
+        'St alban': 1987,
+        'Belleville': 1989,
+        'Blayais': 1983,
+        'Cruas': 1985,
+        'Fessenheim': 1978,
+        'Flamanville': 2005,  # new reactor being built, others 1987
+        'Dampierre': 1981,
+        'Chinon': 1988,
+        'Bugey': 1980,
+        'Cattenom': 1992,
+        'Chooz': 2000,
+        'Civaux': 2002,
+        'Tricastin': 1981,
+        'Dukovany': 1987,
+        'Temelín': 2003,
+        'Bohunice': 1985,
+        'Mochovce': 2000,
+        'Krško': 1983,
+        'Ringhals': 1983,
+        'Oskarshamn': 1985,
+        'Forsmark': 1985,
+        'Olkiluoto': 2019}
+
+    Capacity = {
+        'St laurent': 1830,
+        'Gravelines': 5460,
+        'Paluel': 5320,
+        'Penly': 2660,
+        'Nogent': 2620,
+        'Golfech': 2620,
+        'St alban': 2670,
+        'Belleville': 2620,
+        'Blayais': 3640,
+        'Cruas': 3660,
+        'Fessenheim': 0,
+        'Flamanville': 4260,  # incl. new reactor being built
+        'Dampierre': 3560,
+        'Chinon': 3620,
+        'Bugey': 3580,
+        'Cattenom': 5200,
+        'Chooz': 3000,
+        'Civaux': 2990,
+        'Tricastin': 3660,
+        'Ringhals': 2166,
+        'Oskarshamn': 1400,
+        'Forsmark': 3269,
+        'Dukovany': 1878,
+        'Temelín': 2006,
+        'Bohunice': 943,
+        'Mochovce': 872,
+        'Krško': 688,
+        'Olkiluoto': 1600, #Capacity of the new reactor only
+        'Brokdorf': 0,  # Set German capacitities to zero
+        'Emsland': 0,
+        'Grohnde': 0,
+        'Gundremmingen': 0,
+        'Isar': 0,
+        'Neckarwestheim': 0,
+        'Philippsburg': 0,
+        'Biblis': 0,
+        'Unterweser': 0,
+        'Grafenrheinfeld': 0,
+        'Kruemmel': 0}
+
+    nuc = pd.DataFrame(ppl.loc[(ppl.Fueltype == "Nuclear"), ["Name", "DateIn", "Capacity"],])
+    for name, year in iteritems(YearCommercial):
+        name_match_b = nuc.Name.str.contains(name, case=False, na=False)
+        if name_match_b.any():
+            nuc.loc[name_match_b, "DateIn"] = year
+        else:
+            print("'{}' was not found in given DataFrame.".format(name))
+        ppl.loc[nuc.index, "DateIn"] = nuc["DateIn"]
+
+    for name, capacity in iteritems(Capacity):
+        name_match_b = nuc.Name.str.contains(name, case=False, na=False)
+        if name_match_b.any():
+            nuc.loc[name_match_b, "Capacity"] = capacity
+        else:
+            print("'{}' was not found in given DataFrame.".format(name))
+        ppl.loc[nuc.index, "Capacity"] = nuc["Capacity"]
+
+    return ppl
 
 if __name__ == "__main__":
     if 'snakemake' not in globals():
@@ -113,14 +207,22 @@ if __name__ == "__main__":
                     df.Fueltype
                       .where(df.Fueltype != 'Natural Gas',
                              df.Technology.replace('Steam Turbine',
-                                                   'OCGT').fillna('OCGT')))))
+                                                   'OCGT').fillna('OCGT'))))
+           .assign(DateIn=lambda df: (
+                    df.DateIn
+                      .where((df.Country == 'SE') & (df.Fueltype == 'Nuclear'),
+                             df.DateIn.replace('2006', '1985'))))
+           )
 
+    #
     ppl_query = snakemake.config['electricity']['powerplants_filter']
     if isinstance(ppl_query, str):
         ppl.query(ppl_query, inplace=True)
 
     ppl = add_custom_powerplants(ppl) # add carriers from own powerplant files
-
+    print(ppl)
+    ppl = correct_nuclear_data(ppl)
+    print(ppl)
     cntries_without_ppl = [c for c in countries if c not in ppl.Country.unique()]
 
     for c in countries:
